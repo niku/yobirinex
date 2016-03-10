017cad934112911d2f1d9f5705fc3d7a1cb996d8
niku <niku@niku.name>
2016-03-10 19:48:41 +0900

    Make code simple

--- lib/yobirinex.ex
+++ lib/yobirinex.ex
@@ -1,7 +1,20 @@
 defmodule Yobirinex do
   use Application
 
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
   def start(_type, _args) do
-    Yobirinex.Supervisor.start_link([])
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Define workers and child supervisors to be supervised
+      # worker(Yobirinex.Worker, [arg1, arg2, arg3]),
+      Plug.Adapters.Cowboy.child_spec(:http, Yobirinex.Handler, [configdir: "~/configdir"], [])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Yobirinex.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
